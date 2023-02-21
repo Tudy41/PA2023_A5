e710f31919c359bed3036e3e4ccd0aa31ad7ea2f
Tudy41 <78901282+Tudy41@users.noreply.github.com>
2023-02-21 17:39:49 +0200

    Add files via upload

---
+++ Lab1/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!\n");
+        String[] languages= {"C", "C++", "C#", "Python", "Go", "Rust", "JavaScript", "PHP", "Swift", "Java"};
+         int n = (int) (Math.random() * 1_000_000);
+        System.out.println("Numarul este "+n+"\n");
+        n=n*3;
+        n+=21;
+        n+=255;
+        n=n*6;
+        System.out.println("Numarul este "+n+"\n");
+        int s=0;
+
+
+        int result=0;
+        System.out.println("Willy-nilly, this semester I will learn " + languages[result]);
+
+    }
+
+
+}
